dde2f0cbfd3cf1e0b39fa99efd597a06efe944ee
Eric Millington <eric.millington@gmail.com>
2014-03-28 17:32:43 -0400

    Added some logic to the models

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,2 +1,6 @@
 class Offer < ActiveRecord::Base
+  belongs_to :purchaser, foreign_key: "purchaser_id", class_name: "User"
+  belongs_to :trade, foreign_key: "trade_id", class_name: "Trade"
+
+  serialize :offered
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,3 +1,9 @@
 class Trade < ActiveRecord::Base
-  belongs_to :user, foreign_key: "poster_id", class_name: "User"
+  belongs_to :poster, foreign_key: "poster_id", class_name: "User"
+  belongs_to :purchaser, foreign_key: "purchaser_id", class_name: "User"
+  has_many :offers, class_name: "Offer"
+
+  serialize :offered
+  serialize :requested
+  serialize :received
 end
