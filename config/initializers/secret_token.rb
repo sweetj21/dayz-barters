8e74ebdd0d088912fd2b9c0fb94c931a1a344244
Eric Millington <eric.millington@gmail.com>
2014-03-28 12:23:13 -0400

    Initialize new repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DayzBarters::Application.config.secret_key_base = 'b31c3e6727a1495e4eab20d8223b71fe25e6c699177d7e744ea32aa2c50f33ecc5d7e31eb10992286d620acb56ddfbdd4d5a0b53a8e7ca0533277701d043c9b2'
