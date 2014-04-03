62e49fa03c3d25b003922081e155c7837918ca0c
Eric Millington <eric.millington@gmail.com>
2014-04-03 15:22:52 -0400

    Importing and Exporting CSV and Excel works

--- app/models/loot.rb
+++ app/models/loot.rb
@@ -22,8 +22,10 @@ class Loot < ActiveRecord::Base
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
     when ".csv" then Roo::Csv.new(file.path)
-    when ".xls" then ::Excel.new(file.path, nil, :ignore)
-    when ".xlsx" then ::Excelx.new(file.path, nil, :ignore)
+    when ".xls" then Roo::Spreadsheet.open(file.path, extension: :xls)
+    #when ".xls" then Roo::Excel.new(file.path, extension: :xls)
+    when ".xlsx" then Roo::Spreadsheet.open(file.path, extension: :xlsx)
+    #when ".xlsx" then Roo::Excelx.new(file.path)
     else raise "Unknown file type: #{file.original_filename}"
     end
   end
